4a9413f0560b6ae6779f614c5a787785db7b4cb8
Connor Mitchener <lightsoutconnor1@gmail.com>
2023-01-08 14:23:27 -0800

    stylesheet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import Header from './components/Header'
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello from React</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+
+
 
 export default App;
