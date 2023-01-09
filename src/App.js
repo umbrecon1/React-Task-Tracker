d53dab4f40f8415c53d062d2a7a762fd0c1bd8cb
Connor Mitchener <lightsoutconnor1@gmail.com>
2023-01-09 11:11:01 -0800

    delete task button

--- src/App.js
+++ src/App.js
@@ -23,11 +23,16 @@ function App() {
 },
 
 ])
+// Delete Task //
+const deleteTask = (id) => {
+  console.log('delete', id)
+
+}
   
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
