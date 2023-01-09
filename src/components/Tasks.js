f7f9b8b5f5df3a1f95bc7d900fdc95dd3ebba5f0
Connor Mitchener <lightsoutconnor1@gmail.com>
2023-01-08 16:07:16 -0800

    added tasks props

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,31 @@
-import React from "react";
+import {useState} from 'react'
+
 
 const Tasks = () => {
-    return (
-        <div>
+    const [tasks, setTasks] = useState([{
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
 
-        </div>
+    ])
+    return (
+        <>
+         {tasks.map((task) => <h3>{task.text}</h3>)}
+        </>
     )
 }
 
