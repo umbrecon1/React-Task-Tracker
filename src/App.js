6ca7c9b1940373cb0569bef9b31030a63964cdeb
Connor Mitchener <lightsoutconnor1@gmail.com>
2023-01-09 16:34:43 -0800

    close form menu functional and add tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
 import AddTask from './components/AddTask'
+import { useState } from 'react'
 
-function App() {
+
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState
+  (true)
   const [tasks, setTasks] = useState([{
     id: 1,
     text: 'Breakfast',
@@ -50,22 +53,39 @@ function App() {
   
 
   ])
+
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+
+  }
   // Delete Task //
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
 
   }
 
-  // Toggle Reminder
+  // Toggle Reminder //
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id ===id ? {...task, reminder: ! task.reminder} : task))
+    setTasks(
+      tasks.map((task) =>
+       task.id === id ? { ...task, reminder: 
+        !task.reminder } : task
+        )
+    )
   }
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'}
+      <Header onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete={deleteTask} 
+      onToggle={toggleReminder} />) : ('No Tasks')}
     </div>
   );
 }
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,22 +1,41 @@
+import { useState } from 'react'
 
+const AddTask = ({onAdd}) => {
+    const [text, setText] = useState('')
+    const [time, setTime] = useState('')
+    const [reminder, setReminder ] = useState(false)
 
-const AddTask = () => {
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Please add a task')
+            return
+        }
+        onAdd({ text, time, reminder })
+
+        setText('')
+        setTime('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task'/>
+                <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)} />
             </div>
             <div className='form-control'>
                 <label>Time</label>
-                <input type='text' placeholder='Add Time'/>
+                <input type='text' placeholder='Add Time' value={time} onChange={(e) => setTime(e.target.value)} />
             </div>
-            <div className='form-control'>
+            <div className='form-control form-control-check '>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
 
-            <input type='submit' value='Save Task' />
+            <input type='submit' value='Save Task'
+            className="btn btn-block" />
         </form>
     )
 }
